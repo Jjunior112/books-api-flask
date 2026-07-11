70aa05c1589aca45f5b435fdf5f0ef6568ef1846
juarez <juarezgcjr@gmail.com>
2026-07-11 00:14:46 -0300

    feat: add user entity

---
+++ models/user.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+
+from database.database import db
+
+
+class User(db.Model):
+
+    __tablename__ = "users"
+
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+
+    name = db.Column(
+        db.String(100),
+        nullable=False
+    )
+
+
+    email = db.Column(
+        db.String(150),
+        nullable=False,
+        unique=True
+    )
+
+
+    password = db.Column(
+        db.String(255),
+        nullable=False
+    )
+
+
+    created_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow
+    )
